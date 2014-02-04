f27526875af52952eba9abc3100573d875907d8c
Van Looy Detlev <detlev.vanlooy@student.ua.ac.be>
2014-02-04 14:42:11 +0100

    debug tool modification part1
    Implemented showSimilarity same way as displayRelevantVisualWords was
    implemented in original project., renamed it to showWord.

--- findKClosestWords.m
+++ findKClosestWords.m
@@ -1,18 +1,13 @@
-function vocids = findKClosestWords(imageID1, imageID2, histograms, k, nozeroes)
-% finds K words on which two images are most similar. (smallest difference)
+function vocids = findKClosestWords(imageID1, imageID2, histograms, k)
+% finds K words on which two images are most similar. (smallest difference in frequency)
 % returns the indexes in the vocabulary of these words.
 
 im1 = histograms(:,imageID1);
 im2 = histograms(:,imageID2);
-%TODO: zeros
+
 d = (im1 - im2).^2;
 
 % find indexes of minimum value(s)
 [sorted, ids] = sort(d);
 
-if nozeroes
-    zeros = sum(sorted(:)==0);
-    vocids = ids(zeros+1:k+zeros);
-else
-    vocids = ids(1:k);
-end
+vocids = ids(1:k);
---
+++ showWord.m
@@ -0,0 +1,70 @@
+function showWord(imageID, names, vocid, vocabulary)
+% creates a figure containing the image and displays its keypoints
+% which correspond to the descriptors which were projected to the
+% visual word with id = vocid
+
+im = imread(names{imageID});
+im0 = standardizeImage(im);
+
+numWords = size(vocabulary.words,2);
+
+width = size(im,2);
+height= size(im,1);
+[keypoints, descriptors] = computeFeatures(im);
+[words, distances] = quantizeDescriptors(vocabulary, descriptors);
+
+% find the visual words equal to word vocid
+inds = find(words == vocid) ;
+if isempty(inds), 
+    warning('This word does not occur in this image.');
+    return;
+end
+
+[drop, perm_] = sort(distances(inds), 'descend') ;
+perm_ = vl_colsubset(perm_, 25*25, 'beginning') ;
+
+patches = cell(1, numel(perm_)) ;
+for j = 1:numel(perm_)
+u0 = keypoints(1,inds(perm_(j))) ;
+v0 = keypoints(2,inds(perm_(j))) ;
+s0 = keypoints(4,inds(perm_(j))) ;
+
+delta = round(s0*2) ;
+u1 = max(1,u0-delta) ;
+u2 = min(size(im0,2),u0+delta) ;
+v1 = max(1,v0-delta) ;
+v2 = min(size(im0,1),v0+delta) ;
+patches{j} = imresize(im0(v1:v2,u1:u2,:),[32 32]) ;
+end
+
+if isempty(patches)
+    warning('Visual word %d no matches found.') ;
+    return;
+end
+
+figure() ; clf ;
+subplot(1,2,1) ;
+composite = cat(4,patches{:}) ;
+composite = max(0,min(1,composite)) ;
+[drop, perm__] = sort(distances(inds(perm_)), 'descend') ;
+composite = composite(:,:,:,perm__) ;
+
+if ndims(composite) > 3
+    vl_imarray(composite) ;
+else
+    image(composite) ;
+end
+
+set(gca,'xtick',[],'ytick',[]) ; axis image ;
+axis image ;
+title(sprintf('Visual word %d ', vocid));
+
+subplot(1,2,2) ;
+imagesc(im0) ; hold on ;
+kp = keypoints([1 2 4], inds) ;
+kp(3,:) = kp(3,:) * 2 ;
+vl_plotframe(kp, 'linewidth', 2) ;
+axis image ;
+
+drawnow ;
+
