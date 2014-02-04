7702eb7737aed7f6a40f8ff738b6e1208f2aa802
Van Looy Detlev <detlev.vanlooy@student.ua.ac.be>
2014-02-04 16:35:02 +0100

    debug tool modification part2
    Made showImageCluster better and easier to use.

--- showImageCluster.m
+++ showImageCluster.m
@@ -1,22 +1,36 @@
-function showImageCluster(imageIDs, names, cid)
+function showImageCluster(imageIDs, names, varargin)
+% Takes a vector of imageIDs and shows the first 36 images.
+% Use showImageCluster(..., 'numImages', N) to change the amount of 
+% images displayed.
+% Use showImageCluster(..., 'batch', K) to display the Kth batch of
+% 36 images.
+
+opts.numImages = 36;
+opts.batch = 1 ;
+opts = vl_argparse(opts,varargin);
 
 figure;
-cluster = imageIDs(:,cid);
+cluster = imageIDs;
 
 % remove zeros
 cluster(cluster==0) = [];
 
-% for creating the subplots
-tempx = ceil(sqrt(length(cluster)));
-tempy = ceil(length(cluster)/tempx);
+% which images to show
+low = (opts.batch-1) * opts.numImages + 1; % if batch = 1 => start from image 1
+high = (opts.batch-1) * opts.numImages + opts.numImages; % if batch = 1 => end at image numImages
 
-for i = 1:length(cluster)  
-    imID = cluster(i);
+if low > length(cluster),
+   warning('There are only %i images in this cluster.', length(cluster));
+   return;
+end
+high = min(high,length(cluster));
+amount = high - low + 1;
+
+for i = 1:amount
+    imID = cluster(low + i -1);
     im = imread(names{imID});
     im = standardizeImage(im);
    
-    subplot(tempx,tempy,i);
+    vl_tightsubplot(amount,i);
     imshow(im);
 end
-
-subplotsqueeze(gcf, 1.3);
